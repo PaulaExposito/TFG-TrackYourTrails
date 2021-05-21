1f02385ad4f775c54fd79b0ea15c2a1ca63ffa58
PaulaExposito <peexposito@gmail.com>
2021-05-21 22:50:47 +0100

    Deploying frontend to Heroku

--- frontend/src/boot/axios.js
+++ frontend/src/boot/axios.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 
 Vue.prototype.$axios = axios
 
-const api = axios.create({ baseURL: 'http://localhost:3000/api'})
+// const api = axios.create({ baseURL: 'http://localhost:3000/api'})
+const api = axios.create({ baseURL: 'https://tytbackend.herokuapp.com/api'})
 Vue.prototype.$api = api
 
 export { axios, api }
